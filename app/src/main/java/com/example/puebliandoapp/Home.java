e43f59fadff0d512618312306fb498743d883d75
Melissa Jaramillo <edisjara.2003@gmail.com>
2023-06-27 20:42:49 -0500

    Se adjunta audio, se trabaja con clases en java_Home

--- app/src/main/java/com/example/puebliandoapp/Home.java
+++ app/src/main/java/com/example/puebliandoapp/Home.java
@@ -3,12 +3,34 @@ package com.example.puebliandoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Home extends AppCompatActivity {
+
+    //ZONA DE LOS ATRIBUTOS
+
+    Button botonHoteles;
+    Button botonRestaurantes;
+    Button botonSitios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        botonHoteles=findViewById(R.id.botonhoteles);
+        botonRestaurantes=findViewById(R.id.botonrestaurantes);
+        botonSitios=findViewById(R.id.botonsitios);
+
+        //DETECTANDO EVENTOS
+        botonHoteles.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Home.this, "Play Click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
